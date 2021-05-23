271b69ce46f08b89b2696e58bbb48f8dbb0a13c7
cledson-leite <csbetsonline@gmail.com>
2021-05-23 13:06:06 -0300

    Grid padrao da aplicaçao

---
+++ frontend/src/App/index.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import GlobalStyle from "./style";
+import React from 'react'
+
+function App(props) {
+    return (
+        <GlobalStyle>
+            Estudo de Caso Cadastro de Usuários
+        </GlobalStyle>
+    )
+}
+
+export default App
