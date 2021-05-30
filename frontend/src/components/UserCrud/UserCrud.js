d53bdb58fd83985aff5acac4f784e9ed7954cda0
cledson-leite <csbetsonline@gmail.com>
2021-05-30 17:12:13 -0300

    Criaçao do Crud

--- frontend/src/components/UserCrud/UserCrud.js
+++ frontend/src/components/UserCrud/UserCrud.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import Main from "../../layout/Main";
-import { getAll } from '../../services/Axios';
+import { getAll, removeOne, update } from '../../services/Axios';
 
 const headerProps = {
     icon: 'users',
@@ -16,11 +16,38 @@ const initialState = {
 
 export class UserCrud extends Component {
     state = {...initialState}
-
+    
     async componentDidMount(){
         const resp = await getAll()
         this.setState({list: resp })
     }
+    
+    getUpdateList(user, add = true) {
+        const list = this.state.list.filter( u => u.id !== user.id)
+        if (add) list.unshift(user)
+        this.setState({ list }) 
+    }
+    clear() {
+        this.setState({user: initialState.user}) 
+    } 
+
+    load(user) {
+        this.setState({user}) 
+    } 
+
+    async save() {
+        const user = this.state.user
+        const resp = await update(user)
+        const list = this.getUpdateList(resp) 
+        this.setState({user: initialState, list}) 
+    }
+
+    async remove(user) {
+        await removeOne(user)
+        const list = this.getUpdateList(user, false) 
+        this.setState({ list }) 
+    }
+
 
     render() {
         return (
